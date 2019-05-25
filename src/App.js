1e216239636bdc8aa5dd4de125fd095d06b26d80
Matt Ackerman <matt@matts-mbp.home>
2019-05-24 18:21:36 -0600

    fixed a bunch of stuff.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 //import * as handleForm from './HandleForm.js'
 //import logo from './logo.svg';
 //import './App.css';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 // the class that includes reactjs logic as well as renders the html page
 class PostForm extends Component {
@@ -11,12 +12,27 @@ class PostForm extends Component {
   constructor(props) {
     super(props)
     this.listOfOfficials = []
-    this.address = ''
+    this.state = {
+      streetName: '',
+      cityName: '',
+      stateName: ''
+    }
   }
 
   // this is needed to display what the user is typing in the search box
-  handleAddressChange = event => {
-    this.address = event.target.value
+  handleStreetChange = event => {
+    event.preventDefault()
+    this.setState({streetName: event.target.value});
+  }
+
+  handleCityChange = event => {
+    event.preventDefault()
+    this.setState({cityName: event.target.value});
+  }
+
+  handleStateChange = event => {
+    event.preventDefault()
+    this.setState({stateName: event.target.value});
   }
 
   // on submit of the address search, we use axios to post the http request to spring
@@ -26,7 +42,9 @@ class PostForm extends Component {
     });
 
     event.preventDefault()
-    instance.post('/address?address=' + this.address)
+    instance.post('/address?street=' + this.state.streetName +
+      '&city=' + this.state.cityName +
+      '&state=' + this.state.stateName)
     .then(response => {
       this.listOfOfficials = response.data
       this.forceUpdate();
@@ -38,18 +56,31 @@ class PostForm extends Component {
 
   // displays the html
   render() {
-    const address = this.state
+    const { streetName, cityName, stateName } = this.state
     const firstResult = this.listOfOfficials[0]
 
     // html notes: (object || [].value) checks if it exists before accessing value
+    /*<p>{(firstResult || []).name}</p> <p>{(firstResult || []).officeName}</p>*/
     return (
         <div>
           <form onSubmit={this.handleSubmit}>
-				    <label>Address </label>
-				    <input type="text" value={address} onChange={this.handleAddressChange}/>
+				    <label>Street </label>
+				    <input type="text" value={streetName} onChange={this.handleStreetChange}/>
+            <label>City </label>
+            <input type="text" value={cityName} onChange={this.handleCityChange}/>
+            <label>State </label>
+            <input type="text" value={stateName} onChange={this.handleStateChange}/>
             <button type="submit">Submit</button>
           </form>
-          <p>{(firstResult || []).name}</p> <p>{(firstResult || []).officeName}</p>
+
+          <BootstrapTable data={this.listOfOfficials}>
+            <TableHeaderColumn isKey headerAlign='center' dataField='name'>
+              Name
+            </TableHeaderColumn>
+            <TableHeaderColumn headerAlign='right' dataField='officeName'>
+              OfficeName
+            </TableHeaderColumn>
+          </BootstrapTable>
         </div>
     );
   }
